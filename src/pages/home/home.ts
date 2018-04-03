952b9561ddebbeaaaaff3c286c393ab62641cf1f
Paman <pojsaporn@gamil.com>
2018-04-03 14:09:57 +0700

    my ionic commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private code: number;
+  private name: string;
+  private email: string;
+  private edu: { major: string, faculty: string, university: string };
+  private skills: string[];
 
   constructor(public navCtrl: NavController) {
+    this.code = 57920642;
+    this.name = "Mr.podsaporn Saelim";
+    this.email = "podsaporn.sa@buu.ac.th";
+    this.edu = { major: "IT", faculty: "Informatics", university: "Burapha University" };
+    this.skills = ["HTML", "CSS", "JS", "PHP", "SQL"];
+  }
 
+  addSkill(val_skill) {
+    this.skills.unshift(val_skill);
+    return false;
+  }
+
+  removeSkill(skill) {
+    this.skills.forEach((element, index) => {
+      if (element == skill) {
+        this.skills.splice(index,1); 
+      }
+    });
   }
 
 }
---
+++ src/providers/userdata/userdata.ts
@@ -0,0 +1,24 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the UserdataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UserdataProvider {
+
+  constructor(public http: Http) {
+    console.log('Hello UserdataProvider Provider');
+  }
+
+  get_user() {
+    let url = "https://jsonplaceholder.typicode.com/users";
+    return this.http.get(url).map((res)=>res.json());
+    // console.log(this.http.get(url));
+  }
+
+}
